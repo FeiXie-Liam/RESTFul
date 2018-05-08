dee4385680063b3629c96a98659b5ea86e8b97d6
FeiXie-Liam <xiefei0887@163.com>
2018-05-08 18:11:12 +0800

    implement POST request.

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,13 +1,15 @@
 package com.example.employee.restfulapi.controller;
 
 import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,37 @@ public class CompanyController {
     CompanyRepository companyRepository;
     //在此处完成Company API
     @RequestMapping(method = RequestMethod.GET)
-    List<Company> get(){
+    public List<Company> get(){
         return companyRepository.findAll();
     }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Company getCompanyById(@PathVariable("id") Long id){
+        return companyRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/{id}/employees",method = RequestMethod.GET)
+    public List<Employee> getEmployeesByCompanyId(@PathVariable("id") Long id){
+        return companyRepository.findById(id).getEmployees();
+    }
+
+    @RequestMapping(value = "/page/{page}/pageSize/{pageSize}",method = RequestMethod.GET)
+    public Page<Company> getEmployeesByCompanyId(@PathVariable("page") int page, @PathVariable("pageSize") int pageSize){
+        return companyRepository.findAll(new PageRequest(page, pageSize));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public List<Company> add(@RequestParam String companyName,
+                      @RequestParam Integer employeesNumber){
+        Company company = new Company(companyName,employeesNumber);
+        companyRepository.save(company);
+        return get();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Company putCompany(@PathVariable Long id, @RequestParam String companyName){
+        companyRepository.setNameFor(companyName, id);
+        return companyRepository.findById(id);
+    }
+
 }
--- src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
@@ -2,10 +2,17 @@ package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Company findById(Long id);
+
+    @Modifying
+    @Query("update Company c set c.id=?1 where c.name=?2")
+    int setNameFor(String changedName, Long id);
 }
